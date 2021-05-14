aaddbca771d6ef334751cff7c04f182ab79e8ef6
deformal <drake9211@gmail.com>
2021-05-14 20:12:00 +0530

    sql views implemented

--- src/base/main.src.js
+++ src/base/main.src.js
@@ -1,70 +1,82 @@
-const {app, BrowserWindow, ipcMain} = require('electron')
-const bytenode = require('bytenode')
-const sqlite3 = require('@journeyapps/sqlcipher').verbose()
-const path  = require('path')
+const { app, BrowserWindow, ipcMain } = require("electron");
+const bytenode = require("bytenode");
+const sqlite3 = require("@journeyapps/sqlcipher").verbose();
+const path = require("path");
 
-let db = new sqlite3.Database('test.db', (err) => {
+let db = new sqlite3.Database("test.db", (err) => {
   if (err) {
     return console.error(err.message);
   }
-  console.log('Connected to the SQlite database.');
+  console.log("Connected to the SQlite database.");
 });
-db.serialize(function(){
+db.serialize(function () {
   db.run("PRAGMA cipher_compatibility = 4");
   db.run("PRAGMA key = 'mysecret'");
-  db.run("CREATE TABLE contact (name TEXT, number NUMERICAL)",(err)=>{
-    
-    if (err) console.log("table already created")
-
-  });
-})
+  db.run(
+    "CREATE TABLE person (pid INTEGER NOT NULL UNIQUE ,name TEXT NOT NULL, PRIMARY KEY('pid' AUTOINCREMENT))",
+    (err) => {
+      if (err) console.log(err);
+      console.log("table person create");
+    }
+  );
+  db.run(
+    "CREATE TABLE phone (pid INTEGER NOT NULL UNIQUE ,phonenumber NUMERICAL NOT NULL UNIQUE, PRIMARY KEY('pid' AUTOINCREMENT) )",
+    (err) => {
+      if (err) console.log(err);
+      console.log("table phone created");
+    }
+  );
+});
 
 let mainwindow;
 
 const createWindow = () => {
-    mainwindow = new BrowserWindow({
-        width:500,
-        height:500,
-        webPreferences:{
-            nodeIntegration:true,
-            contextIsolation:false,
-        }
-    })
-   mainwindow.loadFile(__dirname+"/index.html");
-}
+  mainwindow = new BrowserWindow({
+    width: 500,
+    height: 500,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+  mainwindow.loadFile(__dirname + "/index.html");
+};
 
-app.on('ready',createWindow)
+app.on("ready", createWindow);
 
-
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
-    }
-  })
-
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
   }
-})
+});
 
-ipcMain.on('addContacts',async (event, data) => {
- const intoquery = `INSERT INTO contact (name, number) VALUES ('${data.x}','${data.y}')`
- db.run(intoquery)
- event.sender.send("addContact","added")
-console.log(`added ${data.name} to the contacts list`)
-})
-
-ipcMain.on('showContacts',async (event) => {
-  const query ='SELECT * FROM contact'  
- db.each(query, (err, row) => {
-  if (err) {
-    console.log(err);
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
   }
-  event.sender.send('showContact',row)
-})
+});
 
-ipcMain.on('fromhtml',async (event)=>{
-  event.sender.send("fromserver","hello")
-})
-})
+ipcMain.on("addContacts", async (event, data) => {
+  const personinsert = `INSERT INTO person (name) VALUES ('${data.x}')`;
+  const contactinsert = `INSERT INTO phone (phonenumber) VALUES ('${data.y}')`;
+  db.run(personinsert);
+  db.run(contactinsert);
+  event.sender.send("addContact", "added");
+  console.log(`added ${data.x} with ${data.y} to the contacts list`);
+});
+
+ipcMain.on("showContacts", async (event) => {
+  const query =
+    "CREATE VIEW persons AS SELECT name, phonenumber FROM person INNER JOIN phone ON phone.pid = person.pid";
+  const statement = db.exec(query, (err) => {
+    err ? console.log(err) : console.log("ok to go");
+  });
+  const viewquery = `SELECT * FROM persons`;
+  db.each(viewquery, (err, row) => {
+    err ? console.log(err) : event.sender.send("showContact", row);
+  });
+
+  ipcMain.on("fromhtml", async (event) => {
+    event.sender.send("fromserver", "hello");
+  });
+});
--- src/base/originalClient.js
+++ src/base/originalClient.js
@@ -1,45 +1,45 @@
-   const {ipcRenderer} = require('electron')
+const { ipcRenderer } = require("electron");
+// client functions
 function subform() {
-        const x = document.getElementById('con').value
-        const y = document.getElementById('connum').value
-        const h1 = document.querySelector('h1')
-        let usr;
-        if(x == "" || y == "") h1.innerText = "empty fields"
-        else {
-            h1.innerText = "added"
-            usr = {x,y}
-        console.log(usr)
-        ipcRenderer.send('addContacts',usr)
-       setTimeout(function(){window.location.reload() }, 1000);
-    }
+  const x = document.getElementById("con").value;
+  const y = document.getElementById("connum").value;
+  const message = document.getElementById("msg");
+  let usr;
+  if (x == "" || y == "") h1.innerText = "empty fields";
+  else {
+    message.innerText = "added";
+    usr = { x, y };
+    console.log(usr);
+    ipcRenderer.send("addContacts", usr);
+    setTimeout(function () {
+      window.location.reload();
+    }, 500);
+  }
 }
 
-function showform(){
-        ipcRenderer.send('showContacts')
-        const list = document.getElementById('list')
-        document.getElementById("showbtn").disabled = true
-
-    }
-
-ipcRenderer.on('showContact',(err,result) => {
-        const data = [result]
-        data.forEach(element => {
-            let li = document.createElement('li')
-            li.innerText = element.name;
-            list.appendChild(li)
-        });
-    });
-
-function hey(){
-ipcRenderer.send('fromhtml')
-
+function showform() {
+  ipcRenderer.send("showContacts");
+  const list = document.getElementById("list");
+  document.getElementById("showbtn").disabled = true;
 }
-ipcRenderer.on("addContact",(err, data)=>{
-        document.querySelector('h1').innerText = data
-        setTimeout(function(){document.querySelector('h1').innerText = "" }, 500);
 
-    })
- ipcRenderer.on("fromserver",async(err,data)=>{
-       const x = document.querySelector('h1')
-       x.innerText = data
-    })
+// ipc functions
+ipcRenderer.on("showContact", (err, result) => {
+  const data = [result];
+  data.forEach((element) => {
+    let li = document.createElement("li");
+    li.innerText = element.name;
+    list.appendChild(li);
+  });
+});
+
+ipcRenderer.on("addContact", (err, data) => {
+  document.querySelector("h1").innerText = data;
+  setTimeout(function () {
+    document.querySelector("h1").innerText = "";
+  }, 500);
+});
+ipcRenderer.on("fromserver", async (err, data) => {
+  const x = document.querySelector("h1");
+  x.innerText = data;
+});
